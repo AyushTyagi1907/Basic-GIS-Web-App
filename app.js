fb2c183b17497e779af0b2992e50fe880a4864ed
Ayush Tyagi <ayush2k3@gmail.com>
2024-02-05 01:17:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ fileInput.addEventListener('change', function (e) {
 });
 
 // Add GeoJSON upload button to the map
-fileInput.style.position = 'absolute';
-fileInput.style.zIndex = 1000;
-fileInput.style.margin = '10px';
+//fileInput.style.position = 'absolute';
+//fileInput.style.zIndex = 1000;
+//fileInput.style.margin = '10px';
 
 document.getElementById('map').appendChild(fileInput);
 
